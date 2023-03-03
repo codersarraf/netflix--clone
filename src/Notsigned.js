767fd8e75b6b4e4b12c234648792155defc07801
amitkr <amitsarraf6@gmail.com>
2023-03-03 13:13:39 +0530

    initial commit

--- src/Notsigned.js
+++ src/Notsigned.js
@@ -3,11 +3,11 @@ import React from 'react'
 function Notsigned({setModal}) {
   return (
     <div id="notSigned">
-      <div class="freeWatchContainer">
+      {/* <div class="freeWatchContainer">
         <h1>See what's next</h1>  
         <h3>WATCH ANYTIME ANYWHERE</h3>
         <button id="freeWatchBtn" onClick={()=>setModal(true)}>WATCH FREE FOR 30 DAYS</button> 
-      </div>  
+      </div>   */}
     </div>
   )
 }
