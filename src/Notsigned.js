cdad8ab95c0c071d2984306b671000f5eb1f811d
amitkr <amitsarraf6@gmail.com>
2023-03-05 04:08:08 +0530

    initial-commit

--- src/Notsigned.js
+++ src/Notsigned.js
@@ -4,12 +4,14 @@ import netflix_Name from "./image/netflix_Name.jpg"
 function Notsigned({setModal}) {
   return (
     <div id="notSigned">
-      <img src={netflix_Name} className='name_logo_big' />
-      <div class="freeWatchContainer">
-        <h1>See what's next</h1>  
-        <h3>WATCH ANYTIME ANYWHERE</h3>
-        <button id="freeWatchBtn" onClick={()=>setModal(true)}>WATCH FREE FOR 30 DAYS</button> 
-      </div>  
+      <div class="noSignData">
+        <img src={netflix_Name} className='name_logo_big' />
+        <div class="freeWatchContainer">
+          <h1>See what's next</h1>  
+          <h3>WATCH ANYTIME ANYWHERE</h3>
+          <button id="freeWatchBtn" onClick={()=>setModal(true)}>WATCH FREE FOR 30 DAYS</button> 
+        </div>
+      </div>    
     </div>
   )
 }
